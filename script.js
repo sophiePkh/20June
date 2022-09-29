9a9f1ce5d3483c70e9b15a674f2e0a087f600844
sophiko pkhikidze <sophie.pkhikidze@gmail.com>
2022-09-29 08:17:44 +0400

    add info

---
+++ script.js
@@ -0,0 +1,264 @@
+let imageArr = [
+    {
+        id: 0,
+        src:`https://gdb.rferl.org/f3d3d8da-dda8-4296-b388-45d416f58f98.png?fbclid=IwAR39lMumh1hBWkh14kV_krQk2n8kP5DDlanArBiaCXNDogRz8GHBC52BV1E`,
+        justName:`გიო`,
+        name:`გიო სულაშვილი `,
+        age:`30 წლის`,
+        videoSrc:`https://www.youtube.com/embed/jzS8sDKgCnU`,
+        paragraph:`თვალის დაკარგვიდან ერთი წლის შემდეგ გიორგი ამბობს, რომ მისთვის ყველაზე რთული ის დღეები იყო, როცა სამკურნალოდ გერმანიაში მიდიოდა.
+        უკან დაბრუნებულს შვილი კარს უკეტავდა იმის შიშით, რომ კიდევ ხანგრძლივად მოუწევდა მამის  გარეშე ყოფნა.
+        <br><br>
+        აქციაზე 20 ივნისს, 10-ის ნახევარზე, სახლიდან წავიდა და რუსთაველზე თანამშრომლების, ტელეკომპანია „რუსთავი 2“-ის გადამღებ ჯგუფებთან ახლოს ტრიალებდა.
+        სხვადასხვა ჩვენების მიხედვით,  ჟურნალისტები განსაკუთრებული სამიზნეები იყვნენ სამართალდამცველთა ტყვიებისთვის - გიორგის თვალის გარდა   ჩამსხვრეული
+        ჰქონდა სახის ძვლებიც. 4 ოპერაცია უკვე გაიკეთა, წინ კიდევ რამდენიმე   აქვს გასაკეთებელი, სანამ სახე ჩვეულ ფორმას მიიღებს და თვალის პროთეზს მოირგებს.
+        პატარა თეთრი სახვევი თვალზე ერთი წლის განმავლობაში მისი სახის ნაწილად იქცა`
+    },
+    {
+        id: 1,
+        src: `https://gdb.rferl.org/5ae0f04c-4f8b-4065-b18c-35f343cc905e.png?fbclid=IwAR2-PYEEg3LwPGSs_uThOzqRP1k9ERuGxKxRKluTIpv26xIIJb_NNJV1tGw`,
+        name:`მაკო გომური `,
+        age:`19 წლის`,
+        justName:`მაკო`,
+        videoSrc:`https://www.youtube.com/embed/wzQDgvDR5Js`,
+        paragraph: `2019 წლის სექტემბერში მაკომ ყოფილ შინაგან საქმეთა მინისტრს და მოქმედ პრემიერ-მინისტრს, გიორგი გახარიას, წერილი მისწერა.
+        მას აინტერესებდა, რატომ იყო თავისუფალი ადამიანი, რომელმაც მას თვალი გამოსთხარა და რანაირად შეძლებდა ყოფილი  მთავრობის მეთაური ქვეყნის მართვას,
+        თუკი უდანაშაულო ხალხის უსაფრთხოებაზე არ ზრუნავდა. წერილში მაკომ გიორგი გახარიას “ისეთივე სამართლიანი მომავალი უსურვა, როგორიც თავადაა”.
+        იმ დღეს პრემიერ-მინისტრმა თქვა, რომ მაკო გომურთან ურთიერთობას თვითონ გაარკვევდა - 2019 წელს მაკო მზად იყო პრემირ-მინისტრს შეხვედროდა,
+        2020 წელს ამ შეხვედრის აზრს უკვე ვეღარ ხედავს.
+        <br><br>
+        პირველ საპროტესტო აქციაზე  თვალის  დაკარგვის შემდეგ მაკომ  რამდენიმე ოპერაცია გაიკეთა, დაზარალებულის სტატუსი თვეების შემდეგ მიიღო -
+        წინაა სამართლებრივი დავა სახელმწიფოსთან  მორალური და ფიზიკური ზიანის დასადგენად და კომპენსაციის მისაღებად.
+        `
+    },
+    {
+        id: 2,
+        src: `https://gdb.rferl.org/8b657811-891d-4ec9-ad75-b0ada32693e7.png?fbclid=IwAR1QdxEkIMTCg1Zh4a3yvCYTK_aJ_gnNHCA97a2WEUG7V713Pflp4oUpFSU`,
+        name:`კობა ლეთოდიანი`,
+        age:`51 წლის`,
+        justName:`კობა`,
+        videoSrc:``,
+        paragraph: `20 ივნისს მოხვედრილი რეზინის ტყვიები კობას სხეულიდან მხოლოდ ივლისში ისრაელის ერთ-ერთ კლინიკაში ამოიღეს. ის იყო დაშავებულთვალებიანთაგან
+        ყველაზე მძიმე პაციენტი, რომელსაც ქართველმა ექიმებმა თვალის სრულად დაკარგვის შიშით ოპერაცია ვერ გაუკეთეს. მცირე ზომის რეზინის ტყვიებმა, რომელიც კობას აქციაზე მოხვდა,
+        მის ერთადერთ ჯანმრთელ თვალში მხედველობა გააქრეს. კობამ მარჯვენა თვალი აფხაზეთის ომში დაკარგა. 24 წლის იყო, ომი რომ დაიწყო, და მიუხედავად ორჯერ მძიმედ დაჭრისა,
+        სოხუმის დაცემამდე იბრძოდა.
+        <br><br>
+        2020 წლის ივნისში ამბობს, რომ სოხუმის დატოვებისას ვერასოდეს წარმოიდგენდა, თუ მისი მტრების მხარდამჭერი მის ქვეყანაში თავისუფლად შემოვიდოდა და არც იმის სჯეროდა,
+        რომ საკუთარი ქვეყანა დააკარგვინებდა მეორე თვალში მხედველობას.
+        <br><br>
+        სახელმწიფო კობა ლეთოდიანს დაზარალებულად არ თვლის.
+        `
+    },
+    {
+        id: 3,
+        src: `https://gdb.rferl.org/ba0d428f-12bd-4a0c-96b6-8e909228631c.png?fbclid=IwAR3Il5BpjF_k3KgA0O_pALG1VHLcr5-NfeaNGEm8x9P85LJZLOteaCeI160`,
+        name:`დავით ფარადაშვილი `,
+        age:`32 წლის`,
+        justName:`დავითი`,
+        videoSrc:`https://www.youtube.com/embed/_0xYJ1y4xQE`,
+        paragraph: `დავითი 2019 წლის 20 ივნისს სახლიდან ჩვეულებრივ გავიდა, ანტისაოკუპაციო აქციას შეუერთდა და იმის შემდეგ თითქმის მთელი წელი რამდენიმე
+        კლინიკაში მოუწია ცხოვრებამ - სახლში მხოლოდ  2020 წლის გაზაფხულზე დაბრუნდა, როცა ეპიდემიის გამო კლინიკებმა საწოლების გათავისუფლება დაიწყეს.
+        დავითს  რუსთაველზე ცრემლსადენი აირის ყუმბარა თავში მოხვდა და  ისეთი მძიმე ტრავმა მიაყენა, რომ ახლა  პარალიზებული აქვს მარცხენა მხარე.
+        <br><br>
+        დავითს არ აქვს დაზარალებულის სტატუსი; ის არ არის დაკითხული 20 ივნისთან დაკავშირებული გამოძიების მიერ; მიუხედავად შეზღუდული შესაძლებლობისა,
+        არ იღებს ინვალიდის პენსიას და არ იცის საკუთარი დიაგნოზი - შიშობს, მისი “ზედმეტი” აქტიურობის გამო სახელმწიფომ უარი არ თქვას მკურნალობის დაფინანსებაზე.
+        <br><br>
+        დავითი მებაღეა. სანამ  საკუთარი სახლის აივანზე ეტლში ზის, მის ყვავილებს დედა და  მეგობრები უვლიან.  ეს ყვავილები მისი ერთადერთი შემოსავლის წყარო იყო.`
+    },
+    {
+        id: 4,
+        src: `https://gdb.rferl.org/79354151-5f9b-452a-a6c1-22c13a2515ea.png?fbclid=IwAR0oRM4M-XDC99Kx0qBx4GV5bbSfKx-Sa4XkxbIiA87exxgJ6itQYox9dI0`,
+        name:`დავით ქურდოვანიძე`,
+        age:`21 წლის`,
+        justName:`დავითი`,
+        videoSrc:`https://www.youtube.com/embed/Ko_bJgnOaCc`,
+        paragraph: `თითქმის ერთი წელია, რაც დავითს   ფეისბუკის პერსონალურ  გვერდზე  იმ სისხლიანი ჯინსის ჟილეტის ფოტო აქვს დაყენებული, რომელიც აქციის ღამეს ეცვა.
+        20 ივნისს აქციაზე ის რამდენჯერმე მივიდა. ტყვია თვალში გამთენიისას, დაახლოებით დილის 4 საათზე, ოპერის სიახლოვეს მოხვდა. იმ დროს  აქციის სხვა მონაწილეებს
+        ცრემლსადენი აირის კვამლიდან გამოსვლაში ეხმარებოდა.
+        <br><br>
+        5 ოპერაციის შემდეგ, როცა თვალი ძვლის ნამსხვრევებისგან უკვე სრულიად გაწმენდილია და ფორმაც დაიბრუნა,  დავითი დაზიანებული თვალით მხოლოდ სინათლეს ხედავს.`
+    }
+
+]
+let hortName;
+$(document).ready(function() {
+    
+
+    getPersonInfo(imageArr);
+    setTimeout(() => {
+        $(".loader-holder").css("display","none");          
+    }, 1500);
+
+});
+
+
+
+function getPersonInfo(imageArr){
+    let mainDiv=document.getElementsByClassName("main_page");
+    let eachMU;
+    for(let i=0;i<imageArr.length;i++){
+        let eachMU=drawEachPerson(i);
+        $(mainDiv).append(eachMU);
+    }
+
+}
+
+function drawEachPerson(i){
+    if(imageArr[i]){
+        let eachPerson = `
+            <aside class="eachPerson_MU " >
+                <div class="img-hover-zoom"   >
+                    <a  class="click_new_page" id='${i}'>
+                        <img  src="${imageArr[i].src}">
+                    </a>
+                </div>
+                <div class="person_info" >${imageArr[i].name}</div>
+                <div class="person_age" >${imageArr[i].age}</div>
+                <button class="person_button click_new_page" id='${i}' >${imageArr[i].justName}ს ისტორია</button>
+            </aside>
+        `
+        return eachPerson;
+    }
+}
+
+function RemoveLastDirectoryPartOf(){
+    var url= window.location.href;
+    var the_arr = url.split('/');
+    the_arr.pop();
+    let newLocation = the_arr.join('/') + '/personPage.html';
+    return(newLocation);
+}
+
+$(".main_page").on('click', '.click_new_page', function(mainDiv) {
+    let openNewPage=RemoveLastDirectoryPartOf();
+    window.open(`${openNewPage}`, '_self');
+    let currentId=$(this).attr('id');
+    const setCurrentId=localStorage.setItem("currentId",currentId);
+});
+
+function drawPersonalPage(i,currentImg,currentName,currentParagraph,currentAge){
+    let personalPage = `
+        <aside class="left_side">
+            <img class="" src="${currentImg}">
+        </aside>
+        <aside class="right_side">
+            <div class="common-div">
+                <div class="five_story_container grow">
+                    <a href="index.html"><img src="./assets/5story.png"></a>
+                </div>
+                <h2>${currentName} </h2>
+                <h2 class="age_conatiner">${currentAge} </h2>
+                ${currentName=="კობა ლეთოდიანი" ? " ": ` <img id="myBtn" data-id="${i}" src="./assets/naxe.png">`}
+                <div class="article_common_container">
+
+                <aricle class="description" >${currentParagraph}</aricle>
+                <br>
+                ${currentName=="კობა ლეთოდიანი" ? `
+                <a class="read-more-show hide" href="#"><img src="./assets/more.png"></a>
+                    
+                    <span class="read-more-content" id="showOrHideDiv">
+                    <br>მარჯვენა თვალი წლების წინ აფხაზეთში დაუზიანდა - 1992 წლის 12 ოქტომბერს ვერტმფრენს, სადაც 5 მგზავრთან ერთად იჯდა ნაღმტყორცნის ჭურვი მოხვდა - ვერტმფენს “კუდი” მოწყდა და ჩამოვარდა.  კობამ რამდენიმე თვე ჰოსპიტალში გაატარა. სოხუმის დაცემმადე იბრძოდა. ვერასდროს  წარმოიდგენდა, რომ სერგეი გავრილოვი, რომელიც  ფულით, იარაღით,  და ინფორმაციით ებრძოდა საქართველოს ტერიტორიულ მთლიანობას,  27 წლის შემდეგ მის ქვეყანაში თავისუფლად შემოვიდოდა. “ომში შენც ისვრი, სხვაც გესვრის და ეს სხვა მტერია, ერთი წლის წინ პარლამენტთან კი მტრის მაგივრად სახელმწიფო “ებრძოდა” ხალხს”- , ამბობს კობა. 
+                    <br>
+                    <br>50 წლის კობა  20 ივნისს სახლიდან  ორჯერ გავიდა, პირველად დღე, როცა რუსი კომუნისტი დეპუტატის პარლამენტში გამოჩენას სპონტანური პროტესტი მოჰყვა, მეორედ უკვე შებინდებულზე, მეგობართან ერთად ტელევიზორს უყურებდა და სპეცდანიშნულების რაზმები რომ დაინახა, მიხვდა, რომ   პარლამენტის წინ უნდა ყოფილიყო.   
+                    <br>
+                    <br>ბოლო, რაც იმ დღეს დაინახა, სპეცრაზმელი იყო, რომელიც იარაღს უმიზნებდა - ღამის 1 საათი იქნებოდა,  პარლამენტის შენობასთან ახლოს იყო. ტყვია საღ, მარცხენა თვალში მოხვდა და როგორც თვითონ ამბობს, მას შემდეგ ნათურები ჩაქვრა. მხოლოდ ღამით, როცა სახლში შუქს ანთებენ, კობა პატარა და მოციმციმე წერტილებს ხედავს. არ იცის ეს მისი თვალის მეხსიერებაა თუ ცოცხლად გადარჩენილი ნერვის რეფლექსი.  ელოდება, რომ დაზიანებულ თვალში ბადურა გამაგრდება, ჩამოშლის საფრთხე გადაივლის  და მერე შეიძლება ცდა,   მხედველობა  ნაწილობრივ მაინც  დაუბრუნდეს. ძნელია იმის დაჯერება, რომ მხედველობა საბოლოოდ შეიძლება დაიკარგოს. 
+                    <br>
+                    <br>კობას მკურნალობას, ისევე როგორც 20 ივნისს სხვა დაშავებულებისას, ფონდი “ქართუ” ფარავს. სახელმწიფო მას დაზარალებულად არ ცნობს. საქართველოს 
+                    პროკურატურამ 20 ივნისის საქმეზე დაზარალებულად ცნო შსს-ს 78 თანამშრომელი და მხოლოდ 7 სამოქალაქო პირი, კობა ლეთოდიანი მათ შორის ვერ მოხვდა. ეს იმას ნიშნავს, რომ თუკი ერთ დღესაც ფონდი ფინანსურ დახმარებაზე უარს იტყვის, არ იქნება არავინ, ვისაც ექნება მისთვის მიყენებული ზიანის გამოსწორების ვალდებულება. ერთადერთი, რაც სახელმწიფომ  მოახერხა  ინვალიდობის ჯგუფის ცვლილება იყო - ომის ვეტერანი და მეორე ჯგუფის ინვალიდი ახლა პირველი ჯფუფის ინვალიდია, ისიც ხანგრძლივი ბიუროკრატიული პროცედურების შემდეგ. ჯგუფს შეცვლა 30 ლარით მეტ პენსიას ნიშნავს.
+                    <br>
+                    <br>“დადექი სამიზნედ და გესვრით. 9 აპრილზე უარესიც იყო,  იქ რუსი შემოვიდა და აქ შენიანმა გესროლა, ქართველმა. რუსი ყოფილიყო და გადამთიელი, ჯანდაბას. შენი ქვეყანა რომ გკლავს, ეგაა უბედურება. ისიც უბედურებაა, ხალხის ერთობა რომ არაა, იმ ღამეს თუ არა, მეორე დღეს, რომ ნახეს, რაც მოხდა, მთელი ქვეყანა უნდა გამოსულიყო გარეთ. მაგრამ ჩვენი ხალხი ეგრე ყავთ მიჩვეული, რომ ვინმემ თუ არ დაუძახა, არ გამოდის. ამდენი რაღაცის მერე იმას როგორ ვიფიქრებდი, რომ იმ ადგილას სადაც კოსტავამ და გამსახურდიამ დამოუკიდებლობის აქტი გამოაცხადეს, იქ რომ გავრილოვს ჩააჯენდნენ? “ - ამბობს კობა. მას არც ის ეგონა, ოდესმე აფხაზეთში დაბრუნების შესაძლებლობაში ეჭვი თუ შეეპარებოდა. სოხუმიდან ოჯახმა მხოლოდ საოჯახო ფოტოალბომები წამოიღო. სოხუმი რომ ეცემოდა მაშინაც და დევნილობის წლებშიც სულ ჰქონდა იმედი, რომ მალევე უკან დაბრუნდებოდნენ. ახლა ფიქრობს, რომ ასეთ მმართველობაში უკან დაბრუნება შეუძლებელი ჩანს. იმედი, რომელსაც კობა ჯერ კიდევ იტოვებს, დაზიანებულ თვალში სინათლის გამოჩენაა. 
+                    <a class="read-more-hide hide "  href="#"><img src="./assets/less.png"></a></span> `: ""}
+                </div>
+            </div>
+
+        </aside>
+    `
+    return personalPage;
+}
+// 
+
+$(document).ready(function () {
+    
+    let iframeClass=document.querySelector(".iframe");
+    let url = window.location.href;
+   
+
+    url = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.'));
+    if(url === "personPage"){
+       
+        const getCurrentId=parseInt(localStorage.getItem("currentId"));
+        let mainDiv1=document.getElementById("personal_page");
+        let i,currentImg,currentName,currentParagraph,personalMU;
+        for(i = 0; i < imageArr.length; i++){
+            // if(getCurrentId==2){
+            //     $("#myBtn").css("display","none");
+            // }
+            if(getCurrentId==imageArr[i].id){
+                currentImg=imageArr[i].src;
+                currentAge=imageArr[i].age;
+                currentName=imageArr[i].name;
+                currentParagraph=imageArr[i].paragraph;
+                personalMU=drawPersonalPage(i,currentImg,currentName,currentParagraph,currentAge);
+                $(mainDiv1).append(personalMU);
+                showMoreLess();
+
+            }
+        }
+    }
+    modalBoxFunc(iframeClass);
+    
+});
+function showMoreLess(){
+    
+    // Hide the extra content initially, using JS so that if JS is disabled, no problemo:
+    $('.read-more-content').addClass('hide')
+    $('.read-more-show, .read-more-hide').removeClass('hide')
+    
+    // Set up the toggle effect:
+    $('.read-more-show').on('click', function(e) {
+        $(this).next('.read-more-content').removeClass('hide');
+        $(this).addClass('hide');
+        
+        e.preventDefault();
+    });
+    
+    // Changes contributed by @diego-rzg
+    $('.read-more-hide').on('click', function(e) {
+        var p = $(this).parent('.read-more-content');
+        p.addClass('hide');
+        p.prev('.read-more-show').removeClass('hide'); // Hide only the preceding "Read More"
+        e.preventDefault();
+    });
+   
+}
+function modalBoxFunc(iframeClass){
+    let currentDataId=parseInt($("#myBtn").attr("data-id"));
+    let modal = document.getElementById("myModal");
+    let btn = document.getElementById("myBtn");
+    let span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
+        for(i = 0; i < imageArr.length; i++){
+            if(currentDataId==imageArr[i].id){
+                $(iframeClass).attr("src",`${imageArr[i].videoSrc}`);
+            }
+        }
+    }
+    span.onclick = function() {
+        modal.style.display = "none";
+        stopVideo();
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            stopVideo();
+        }
+    }
+}
+
+
+
+function stopVideo() {
+    var currentIframe = document.querySelector('.modal-content > iframe');
+    currentIframe.src = currentIframe.src;
+  }
